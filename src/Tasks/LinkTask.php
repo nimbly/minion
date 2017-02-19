82fec76e544f2793f34b3c3acc78226ff5b0c1e5
Brent Scheffler <brent@brentscheffler.com>
2017-02-18 22:30:28 -0800

    Fixing autoloader for local tasks and commands. Fixing bugs in link and prune.

--- src/Tasks/LinkTask.php
+++ src/Tasks/LinkTask.php
@@ -18,11 +18,9 @@ class LinkTask extends TaskAbstract
         $this->output->writeln("\t<info>Linking new release</info>");
 
         // Remove old symlink
-        $this->output->writeln("Removing old symlink");
         $connection->execute("rm -f {$environment->remote->getCurrentRelease()}");
 
         // Create new symlink
-        $this->output->writeln("Creating new symlink: {$environment->remote->path}");
         $connection->execute("cd {$environment->remote->path}&&ln -s {$environment->remote->getActiveRelease()} {$environment->remote->symlink}");
     }
 }
